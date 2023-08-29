c3983d9fe68c75419bd961397fcfdbd49ad06ce9
Hariharan <hariharanjayachandran04@gmail.com>
2023-08-29 18:43:07 +0530

    Logging switched off

--- db/connection.js
+++ db/connection.js
@@ -7,6 +7,7 @@ const sequelize = new Sequelize({
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  logging: false,
 });
 
 module.exports = sequelize;
