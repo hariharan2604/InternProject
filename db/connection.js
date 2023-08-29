f22104eb63a6e1100e9c0ea165feec5b3c2c2056
Hariharan <hariharanjayachandran04@gmail.com>
2023-08-29 18:40:44 +0530

    DB connection scrutinized

--- db/connection.js
+++ db/connection.js
@@ -7,7 +7,6 @@ const sequelize = new Sequelize({
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  logging: true,
 });
 
 module.exports = sequelize;
